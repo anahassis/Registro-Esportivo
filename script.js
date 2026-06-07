4bd3a7b2a3339910e7cefababa9e0a7cb196c451
Arthur Francisco <arthurfrancisco168@gmail.com>
2026-06-07 18:58:05 -0300

    Completei o index.html, também adicionei um script.js e um estilo.css, mas eles NÃO são definitivos, fiz apenas para me basear enquanto ninguém adicionou nada

---
+++ script.js
@@ -0,0 +1,5 @@
+function mostrarMensagem(){
+    alert("Ainda não concluído");
+}
+
+// Este script NÃO é definitivo, utilizei apenas para a primeira versão, o script só esta linkado com a primeira versão do alterar usuário
